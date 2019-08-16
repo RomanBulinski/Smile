ba2d5cfcd43d35919eadaaf235a11823c4ff920a
Roman Bulinski <aboandrus@gmail.com>
2019-08-16 11:46:44 +0200

    Implemented code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,50 @@
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Main {
 
 
+    static Predicate<String> con1 = i -> i.equals(":)");
+    static Predicate<String> con2 = i -> i.equals(";)");
+    static Predicate<String> con3 = i -> i.equals(":-)");
+    static Predicate<String> con4 = i -> i.equals(";-)");
+    static Predicate<String> con5 = i -> i.equals(":D");
+    static Predicate<String> con6 = i -> i.equals(";D");
+    static Predicate<String> con7 = i -> i.equals(":-D");
+    static Predicate<String> con8 = i -> i.equals(":~D");
+    static Predicate<String> con9 = i -> i.equals(";~D");
+    static Predicate<String> con10 = i -> i.equals(":~)");
+    static Predicate<String> con11 = i -> i.equals(";~)");
+
+
     public static int countSmileys(List<String> arr) {
-        // Just Smile :)
-        return 0;
+
+        arr.stream()
+                .filter(n->n.length()<4)
+                .filter( con1.or(con2)
+                        .or(con3)
+                        .or(con4)
+                        .or(con5)
+                        .or(con6)
+                        .or(con7)
+                        .or(con8)
+                        .or(con9)
+                        .or(con10)
+                        .or(con11)).forEach(m-> System.out.println(m));
+
+        return (int) arr.stream()
+                .filter(n->n.length()<4)
+                .filter( con1.or(con2)
+                        .or(con3)
+                        .or(con4)
+                        .or(con5)
+                        .or(con6)
+                        .or(con7)
+                        .or(con8)
+                        .or(con9)
+                        .or(con10)
+                        .or(con11)).count();
     }
-    
+
+
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,11 +1,26 @@
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest {
+
+
+//    The Array [p-p), ;p), :xX, 5op, ;~d, ;oD, 5-D, 4), pD, ;), :Pp, ;-x, ;D, :D, ;), (), (), :8X, ;)D, pd~D, 8pD, p), ;X, 8-), 5~D, 8-D, 5x2x, 5d), 8-2), p:~D, :2P]
+//    has expected:<4> but was:<5>
+//
+//
+//    The Array [;~d, 4~D, :D, 4), ;), ;p)d, ;-P, ;-d, (D, 5D, ;p, :D, 4~), 4~x, (), p8-p, 5p, 8-X, :), ;o), :D, ;X, 4D, ;d, (-), :), ;-X, 'P, oDp, ~;), '8d, (~X, ;-D), 8X, ;-p, po), od, :), :dP]
+//    has expected:<7> but was:<8>
+//
+//
+//    The Array [:-d, ;8D, ;D, 5~), ;8p, pD, pD, (), 8-), :), :p, 88), ;~D, ~82), 5~P), :p, 88D, 4x, ;-D, 4D, ;dD, ;~), odd, ~5p, -:~d, (8X, :-p, (), 4p2X, :dD, -42p]
+//    has expected:<5> but was:<4>
+
 
     @Test
     public void test1() {
@@ -34,5 +49,25 @@ public class MainTest {
         a.add(":)"); a.add(":)"); a.add("x-]"); a.add(":ox"); a.add(";-("); a.add(";-)"); a.add(";~("); a.add(":~D");
         assertEquals(4, Main.countSmileys(a));
     }
+
+
+
+    @Test
+    public void test6() {
+        List<String> a =  Arrays.asList("p-p)", ";p)", ":xX", "5op", ";~d", ";oD", "5-D", "4)", "pD", ";)", ":Pp", ";-x", ";D", ":D", ";)", "()", "()", ":8X", ";)D", "pd~D", "8pD", "p)", ";X", "8-)", "5~D", "8-D", "5x2x", "5d)", "8-2)", "p:~D", ":2P");
+        assertEquals(4, Main.countSmileys(a));
+    }
+
+
+//    The Array [:X, oD, '-P, o;D, 4~D, :2pDD, '~d, 4xp, ;X, oD, :oD, ;-D, :x, :d, (xx, 85P, ;~), 4~P, o~D, :D, ;8), :o), ;~P, o(), ;), :D, oP, ;~), :D, ~;), :p), 5D), 4p, 8ox), -:), (-P, ;p), ;D, o-Px, ;P]
+//    has expected:<8> but was:<7>
+//
+    @Test
+    public void test7() {
+        List<String> a =  Arrays.asList(":X", "oD'", "'-P", "o;D", "4~D", ":2pDD", "'~d", "4xp", ";X", "oD", ":oD", ";-D", ":x", ":d", "(xx", "85P", ";~)", "4~P", "o~D", ":D", ";8)", ":o)", ";~P", "o()", ";)", ":D", "oP", ";~)", ":D", "~;)", ":p)", "5D)", "4p", "8ox)", "-:)", "(-P", ";p)", ";D", "o-Px", ";P");
+        assertEquals(8, Main.countSmileys(a));
+    }
+
+
 
 }
